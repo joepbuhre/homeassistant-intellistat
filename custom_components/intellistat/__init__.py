52e6d603158f3409e54c0035ea00eb341d57e7a8
Joep <79695868+joepbuhre@users.noreply.github.com>
2024-02-23 21:43:04 +0100

    remove .ha

--- custom_components/intellistat/__init__.py
+++ custom_components/intellistat/__init__.py
@@ -1,15 +1,47 @@
-"""The better_thermostat component."""
-
+"""The intellistat component."""
 import logging
-from asyncio import Lock
-from random import random
-from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant, Config
+
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
+
+from . import const
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass, config):
-    hass.states.async_set("intellistat.world", f"Paulus_{random()}")
-
-    # Return boolean to indicate that initialization was successful.
+    """Track states and offer events for sensors."""
     return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Intellistat integration from a config entry."""
+
+    # _async_import_options_from_data_if_missing(hass, entry)
+
+    hass.data.setdefault(const.DOMAIN, {})
+    hass.data[const.DOMAIN][entry.entry_id] = {}
+
+    # Set up all platforms for this device/entry.
+    await hass.config_entries.async_forward_entry_setups(entry, [Platform.SWITCH])
+
+    # Reload entry when its updated.
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+
+    return True
+
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Reload the config entry when it changed."""
+    await hass.config_entries.async_reload(entry.entry_id)
+
+
+async def async_unload_entry(hass, entry):
+    """Unload Intellistat config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, [Platform.SWITCH])
+
+    if unload_ok:
+        hass.data[const.DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
--- custom_components/intellistat/const.py
+++ custom_components/intellistat/const.py
@@ -1,1 +1,20 @@
+"""Store constants."""
+
+VERSION = "1.1.1"
 DOMAIN = "intellistat"
+NAME = "Intellistat"
+DATA = "data"
+UPDATE_LISTENER = "update_listener"
+
+CONF_CONTROLLER = "controller"
+CONF_ZONES = "zones"
+CONF_MAX_SETPOINT = "max_setpoint"
+CONF_CONTROLLER_DELAY_TIME = "controller_delay_time"
+
+DEFAULT_MAX_SETPOINT = 21
+DEFAULT_CONTROLLER_DELAY_TIME = 10
+
+ATTR_OVERRIDE_ACTIVE = "override_active"
+ATTR_TEMPERATURE_INCREASE = "temperature_increase"
+ATTR_STORED_CONTROLLER_STATE = "stored_controller_state"
+ATTR_STORED_CONTROLLER_SETPOINT = "stored_controller_setpoint"
